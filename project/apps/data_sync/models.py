38dc84cff9116f1c710cc139e2bae952e8f3fa7d
root <crystalmanner130@outlook.com>
2013-10-10 09:51:40 -0700

    models.py updated

--- project/apps/data_sync/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Language(models.Model):
-    language_data = models.TextField(verbose_name="content of language file in base64")
-    dt_created = models.DateTimeField(verbose_name="datetime of language file upload", auto_now_add=True)
