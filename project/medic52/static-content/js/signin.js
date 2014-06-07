4a246066260a32585957ccd60aa4b55f1cf74a0e
root <crystalmanner130@outlook.com>
2014-06-07 14:22:34 -0700

    signin.js updated

--- project/medic52/static-content/js/signin.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('SigninApp', ['ngMaterial']);
-
-app.controller('AppCtrl', ['$scope', function($scope) {
-    $scope.login = function(){
-        angular.element(document.querySelector('#login-form')).submit();
-    }
-}]);
