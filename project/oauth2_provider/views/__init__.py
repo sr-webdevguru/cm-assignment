e5056da3b9c726306386296d891c96fbc964c981
root <crystalmanner130@outlook.com>
2016-04-01 11:22:40 -0700

    __init__.py updated

--- project/oauth2_provider/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .application import ApplicationRegistration, ApplicationDetail, ApplicationList, \
-    ApplicationDelete, ApplicationUpdate
-from .base import AuthorizationView, TokenView, RevokeTokenView
-from .generic import ProtectedResourceView, ScopedProtectedResourceView, ReadWriteScopedResourceView
