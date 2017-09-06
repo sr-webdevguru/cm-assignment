fe24f63c7de47a3aaf823952f83f89a7838ba13a
root <crystalmanner130@outlook.com>
2017-09-06 12:37:49 -0700

    0003_auto_20170617_2345.py added

---
+++ project/apps/routing/migrations/0003_auto_20170617_2345.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('routing', '0002_domains_is_active'),
+    ]
+
+    operations = [
+        # migrations.AddField(
+        #     model_name='domains',
+        #     name='laravel_domain',
+        #     field=models.CharField(max_length=80, default=''),
+        # ),
+    ]
