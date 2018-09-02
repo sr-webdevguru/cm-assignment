5340590099cb9624534eca0c5aa3ee7138edea47
root <crystalmanner130@outlook.com>
2018-09-02 11:29:48 -0700

    0006_merge.py added

---
+++ project/apps/routing/migrations/0006_merge.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('routing', '0005_auto_20170618_2456'),
+        ('routing', '0004_auto_20170617_2352'),
+    ]
+
+    operations = [
+        #migrations.AddField(
+        #    model_name='domains',
+        #    name='laravel_domain',
+        #    field=models.CharField(max_length=80, default=''),
+        #)
+    ]
