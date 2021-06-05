5218a41fbe5f3b828640a8076411658a37dc8d0b
root <crystalmanner130@outlook.com>
2021-06-04 18:00:57 -0700

    dev.py added

---
+++ project/medic52/settings/dev.py
@@ -0,0 +1,116 @@
+from medic52.settings.common import *
+
+# Database setting for local environment
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ.get('DB_PORT', 5432),
+    }
+}
+
+INSTALLED_APPS = COMMON_APPS + ('raven.contrib.django.raven_compat',)
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/data/www/api.dev.medic52.com/static'
+
+TEMPLATE_DIRS += (os.path.join(STATIC_ROOT, 'content'),)
+
+STATICFILES_DIRS = (("project", os.path.join(BASE_DIR, 'medic52/static-content')),)
+
+MEDIA_ROOT = '/data/media/api.dev.medic52.com'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'localhost')
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'LITq2mC3pv3c')
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'duncan@medic52.com')
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_PORT = os.environ.get('EMAIL_PORT', 25)
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
+# EMAIL_USE_TLS = True
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
+# SERVER_EMAIL = EMAIL_HOST_USER
+
+# Configuration for sentry
+RAVEN_CONFIG = {
+    'dsn': 'http://bf692609485343889dabfa171f9a7232:cc1ae1e6098f4160afa9074d03168b2e@sentry.medic52.com/2',
+    'site': 'Medic52 Dev'
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'INFO',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'INFO',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
+
+DEBUG = True
+
+SENDFILE_BACKEND = 'sendfile.backends.nginx'
+SENDFILE_ROOT = os.path.join(MEDIA_ROOT, 'media', 'media')
+SENDFILE_URL = '/media'
+
+SESSION_COOKIE_SECURE = True
+
+RUN_ENV = "dev"
+
+os.environ['HTTPS'] = "on"
+
+TEMPLATE_DIRS += (os.path.join(STATIC_ROOT, 'content'),)
+
+# AWS Settings
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+GLOBAL_KMS_KEY_ID = os.environ['GLOBAL_KMS_KEY_ID']
+AWS_REGION = os.environ['AWS_REGION']
+BUCKET_NAME = os.environ['S3_BUCKET_NAME']
